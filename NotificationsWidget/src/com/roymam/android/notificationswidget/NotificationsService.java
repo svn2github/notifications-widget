72471e2ca311eccaedccf18974e35524f87826f0
leo212@gmail.com <leo212@gmail.com@21922344-3934-ee51-d6e3-fd981ce84c01>
2014-04-12 19:22:29 +0000

    added lock mechanism to notifications list. to prevent reading and writing at the same time.
    git-svn-id: http://notifications-widget.googlecode.com/svn/trunk@306 21922344-3934-ee51-d6e3-fd981ce84c01

--- NotificationsWidget/src/com/roymam/android/notificationswidget/NotificationsService.java
+++ NotificationsWidget/src/com/roymam/android/notificationswidget/NotificationsService.java
@@ -37,6 +37,9 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class NotificationsService extends Service implements NotificationsProvider
 {
@@ -51,11 +54,12 @@ public class NotificationsService extends Service implements NotificationsProvid
     private static NotificationsProvider instance;
     private Context context;
     private NotificationEventListener listener;
-    private ArrayList<NotificationData> notifications = new ArrayList<NotificationData>();
+    private ArrayList<NotificationData> mNotifications = new ArrayList<NotificationData>();
     private NotificationParser parser;
     private HashMap<String, PersistentNotification> persistentNotifications = new HashMap<String, PersistentNotification>();
     private BroadcastReceiver receiver = null;
     private boolean mDirty = true;
+    private ReadWriteLock lock = new ReentrantReadWriteLock();;
     private ArrayList<NotificationData> mFilteredNotificationsList;
 
     public NotificationsService()
@@ -269,65 +273,73 @@ public class NotificationsService extends Service implements NotificationsProvid
             boolean updated = false;
 
             // remove old notification
-            Iterator<NotificationData> iter = notifications.iterator();
+            final Lock w = lock.writeLock();
+            w.lock();
 
             boolean changed = false;
-
             boolean ignoreNotification = false;
 
-            while (iter.hasNext())
+            try
             {
-                NotificationData oldnd = iter.next();
+                Iterator<NotificationData> iter = mNotifications.iterator();
 
-                // remove only if one of the following scenarios:
-                // 1. Android >=4.3 - notification mode is "grouped" and the notification has the same package and id
-                // 2. Android <4.3 - notification mode is "grouped" and the notification has the same package
-                // 3. notification is similar to the old one
-                if (oldnd.packageName.equals(nd.packageName)  &&
-                    (((oldnd.id == nd.id || Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) && notificationMode.equals(SettingsActivity.MODE_GROUPED)) ||
-                     oldnd.isSimilar(nd, true)))
+                while (iter.hasNext())
                 {
-                    nd.uid = oldnd.uid;
-                    nd.deleted = oldnd.deleted;
+                    NotificationData oldnd = iter.next();
 
-                    // protect it from being cleared on next purge command
-                    nd.protect = true;
+                    // remove only if one of the following scenarios:
+                    // 1. Android >=4.3 - notification mode is "grouped" and the notification has the same package and id
+                    // 2. Android <4.3 - notification mode is "grouped" and the notification has the same package
+                    // 3. notification is similar to the old one
+                    if (oldnd.packageName.equals(nd.packageName) &&
+                            (((oldnd.id == nd.id || Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) && notificationMode.equals(SettingsActivity.MODE_GROUPED)) ||
+                                    oldnd.isSimilar(nd, true))) {
+                        nd.uid = oldnd.uid;
+                        nd.deleted = oldnd.deleted;
 
-                    iter.remove();
-                    oldnd.cleanup();
-                    updated = true;
-                    changed = !oldnd.isEqual(nd);
+                        // protect it from being cleared on next purge command
+                        nd.protect = true;
 
-                    // if it is exact the same notificaiton - keep old received time
-                    if (!changed) nd.received = oldnd.received;
+                        iter.remove();
+                        oldnd.cleanup();
+                        updated = true;
+                        changed = !oldnd.isEqual(nd);
 
-                    break;
-                }
-                else // check if the old notification is a duplicate of the current but contains more data than the current - if so - ignore the new one
-                {
-                    if (nd.isSimilar(oldnd, false))
+                        // if it is exact the same notificaiton - keep old received time
+                        if (!changed) nd.received = oldnd.received;
+
+                        break;
+                    } else // check if the old notification is a duplicate of the current but contains more data than the current - if so - ignore the new one
                     {
-                        ignoreNotification = true;
-                        updated = false;
+                        if (nd.isSimilar(oldnd, false))
+                        {
+                            ignoreNotification = true;
+                            updated = false;
+                        }
                     }
                 }
+
+                if (!ignoreNotification)
+                {
+                    // add the new notification
+                    mNotifications.add(nd);
+                    mDirty = true;
+                }
+            }
+            finally
+            {
+                w.unlock();
             }
 
-            if (!ignoreNotification)
-            {
-                // add the new notification
-                notifications.add(nd);
-                mDirty = true;
 
-                // notify that the notification was added
-                if (listener != null && !nd.deleted)
-                {
-                    if (updated)
-                        listener.onNotificationUpdated(nd, changed);
-                    else
-                        listener.onNotificationAdded(nd, true);
-                    listener.onNotificationsListChanged();
-                }
+            // notify that the notification was added
+            if (listener != null && !nd.deleted && !ignoreNotification)
+            {
+                if (updated)
+                    listener.onNotificationUpdated(nd, changed);
+                else
+                    listener.onNotificationAdded(nd, true);
+                listener.onNotificationsListChanged();
             }
         }
     }
@@ -354,40 +366,44 @@ public class NotificationsService extends Service implements NotificationsProvid
             ArrayList<NotificationData> clearedNotifications = new ArrayList<NotificationData>();
 
             // find the notification and remove it
-            Iterator<NotificationData> iter = notifications.iterator();
+            Lock w = lock.writeLock();
+            w.lock();
+            try {
+                Iterator<NotificationData> iter = mNotifications.iterator();
 
-            while (iter.hasNext())
-            {
-                NotificationData nd = iter.next();
+                while (iter.hasNext()) {
+                    NotificationData nd = iter.next();
 
-                if (nd.packageName.equals(packageName) && nd.id == id && !nd.pinned)
-                {
-                    // mark as delete if it's part of multiple events notification
-                    if (logical && nd.event)
-                    {
-                        // mark notification as cleared
-                        nd.deleted = true;
+                    if (nd.packageName.equals(packageName) && nd.id == id && !nd.pinned) {
+                        // mark as delete if it's part of multiple events notification
+                        if (logical && nd.event) {
+                            // mark notification as cleared
+                            nd.deleted = true;
+                        } else if (!nd.deleted) // make sure it hasn't been deleted previously by the user
+                        {
+                            // immediately remove notification
+                            iter.remove();
+                        }
+
+                        mDirty = true;
+
+                        // notify that the notification was cleared
+                        clearedNotifications.add(nd);
+
+                        cleared = true;
+                        // do not stop loop - keep looping to clear all of the notifications with the same id
                     }
-                    else if (!nd.deleted) // make sure it hasn't been deleted previously by the user
-                    {
-                        // immediately remove notification
-                        iter.remove();
-                    }
-
-                    mDirty = true;
-
-                    // notify that the notification was cleared
-                    clearedNotifications.add(nd);
-
-                    cleared = true;
-                    // do not stop loop - keep looping to clear all of the notifications with the same id
                 }
+            }
+            finally
+            {
+                w.unlock();
             }
 
             // notify listener for cleared notifications
             if (cleared && listener != null)
             {
-                for(NotificationData nd : clearedNotifications)
+                for (NotificationData nd : clearedNotifications)
                 {
                     listener.onNotificationCleared(nd);
                 }
@@ -416,7 +432,7 @@ public class NotificationsService extends Service implements NotificationsProvid
     }
 
     @Override
-    public List<NotificationData> getNotifications()
+    public synchronized List<NotificationData> getNotifications()
     {
         if (!mDirty)
             return mFilteredNotificationsList;
@@ -426,13 +442,29 @@ public class NotificationsService extends Service implements NotificationsProvid
         {
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
             String sortBy = prefs.getString(SettingsActivity.NOTIFICATIONS_ORDER, "time");
+            Object[] arr;
 
-            for(NotificationData nd : notifications)
+            Lock r = lock.readLock();
+            r.lock();
+            try
             {
-                if (!nd.deleted)
-                    mFilteredNotificationsList.add(nd);
+                arr = mNotifications.toArray();
             }
-            sortNotificationsList(mFilteredNotificationsList, sortBy);
+            finally
+            {
+                r.unlock();
+            }
+
+            if (arr != null)
+            {
+                for (Object obj : arr)
+                {
+                    NotificationData nd = (NotificationData) obj;
+                    if (!nd.deleted)
+                        mFilteredNotificationsList.add(nd);
+                }
+                sortNotificationsList(mFilteredNotificationsList, sortBy);
+            }
         }
 
         mDirty = false;
@@ -515,21 +547,30 @@ public class NotificationsService extends Service implements NotificationsProvid
 
         ArrayList<NotificationData> clearedNotifications = new ArrayList<NotificationData>();
 
-        Iterator<NotificationData> i = notifications.iterator();
-        while (i.hasNext())
+        Lock w = lock.writeLock();
+        w.lock();
+        try
         {
-            NotificationData nd = i.next();
-            if (!nd.pinned)
+            Iterator<NotificationData> i = mNotifications.iterator();
+            while (i.hasNext())
             {
-                clearedNotifications.add(nd);
+                NotificationData nd = i.next();
+                if (!nd.pinned)
+                {
+                    clearedNotifications.add(nd);
 
-                // if its event - mark it as deleted
-                if (nd.event)
-                    nd.deleted = true;
-                else // otehrwise remove it immediately
-                    i.remove();
-                mDirty = true;
+                    // if its event - mark it as deleted
+                    if (nd.event)
+                        nd.deleted = true;
+                    else // otehrwise remove it immediately
+                        i.remove();
+                    mDirty = true;
+                }
             }
+        }
+        finally
+        {
+            w.unlock();
         }
 
         // notify listener for cleared notifications
@@ -590,22 +631,31 @@ public class NotificationsService extends Service implements NotificationsProvid
         for(String packageName : packages)
         {
             Log.d("NiLS","NotificationsService:clearNotificationsForApps " + packageName);
-            ListIterator<NotificationData> i = notifications.listIterator();
-            while (i.hasNext())
+            Lock w = lock.writeLock();
+            w.lock();
+            try
             {
-                NotificationData nd = i.next();
-                if (!nd.pinned && nd.packageName.equals(packageName))
+                ListIterator<NotificationData> i = mNotifications.listIterator();
+                while (i.hasNext())
                 {
-                    // mark notification as deleted
-                    if (nd.event)
-                        nd.deleted = true;
-                    else
-                        i.remove();
+                    NotificationData nd = i.next();
+                    if (!nd.pinned && nd.packageName.equals(packageName))
+                    {
+                        // mark notification as deleted
+                        if (nd.event)
+                            nd.deleted = true;
+                        else
+                            i.remove();
 
-                    mDirty = true;
-                    changed = true;
-                    clearedNotifications.add(nd);
+                        mDirty = true;
+                        changed = true;
+                        clearedNotifications.add(nd);
+                    }
                 }
+            }
+            finally
+            {
+                w.unlock();
             }
         }
         // notify listener for cleared notifications
@@ -637,39 +687,59 @@ public class NotificationsService extends Service implements NotificationsProvid
         boolean syncback = prefs.getBoolean(SettingsActivity.SYNC_BACK, SettingsActivity.DEFAULT_SYNC_BACK);
 
         // first, find it on list
-        Iterator<NotificationData> iter = notifications.iterator();
         boolean removed = false;
         NotificationData removedNd = null;
 
-        while (iter.hasNext() && !removed)
+        Lock w = lock.writeLock();
+        w.lock();
+        try
         {
-            NotificationData nd = iter.next();
-            if (nd.uid == uid)
+            Iterator<NotificationData> iter = mNotifications.iterator();
+
+            while (iter.hasNext() && !removed)
             {
-                // store id and package name to search for more notifications with the same id
-                removedNd = nd;
+                NotificationData nd = iter.next();
+                if (nd.uid == uid)
+                {
+                    // store id and package name to search for more notifications with the same id
+                    removedNd = nd;
 
-                // mark notification as deleted
-                if (nd.event)
-                    nd.deleted = true;
-                else
-                    iter.remove();
+                    // mark notification as deleted
+                    if (nd.event)
+                        nd.deleted = true;
+                    else
+                        iter.remove();
 
-                mDirty = true;
-                removed = true;
+                    mDirty = true;
+                    removed = true;
+                }
             }
+        }
+        finally
+        {
+            w.unlock();
         }
 
         if (removed)
         {
             // search for more notification with the same id - if not found - dismiss the notification from android status bar
             boolean more = false;
-            for(NotificationData nd : notifications)
+            Lock r = lock.readLock();
+            r.lock();
+            try
             {
-                if (nd.id == removedNd.id && !nd.deleted &&
-                        nd.packageName.equals(removedNd.packageName))
-                    more = true;
+                for (NotificationData nd : mNotifications)
+                {
+                    if (nd.id == removedNd.id && !nd.deleted &&
+                            nd.packageName.equals(removedNd.packageName))
+                        more = true;
+                }
             }
+            finally
+            {
+                r.unlock();
+            }
+
             if (syncback && !more)
                 try
                 {
@@ -688,7 +758,7 @@ public class NotificationsService extends Service implements NotificationsProvid
         }
         else
         {
-            Log.d("NiLS","NotificationsService:clearNotification - wasn't found");
+            Log.d("NiLS", "NotificationsService:clearNotification - wasn't found");
         }
     }
 
@@ -719,23 +789,32 @@ public class NotificationsService extends Service implements NotificationsProvid
 
     private void purgeDeletedNotifications(String packageName, int id)
     {
-        Log.d("NiLS","purging deleted notifications "+ packageName + ":" + id);
+        Log.d("NiLS","purging deleted mNotifications "+ packageName + ":" + id);
 
-        Iterator<NotificationData> iter = notifications.iterator();
-        while (iter.hasNext())
+        Lock w = lock.writeLock();
+        w.lock();
+        try
         {
-            NotificationData nd = iter.next();
-            if (nd.packageName.equals(packageName) &&
-                nd.deleted &&
-               !nd.protect &&
-                (nd.id == id || Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2))
+            Iterator<NotificationData> iter = mNotifications.iterator();
+            while (iter.hasNext())
             {
-                Log.d("NiLS","permanently removing uid:"+nd.uid);
-                iter.remove();
-                mDirty = true;
+                NotificationData nd = iter.next();
+                if (nd.packageName.equals(packageName) &&
+                        nd.deleted &&
+                        !nd.protect &&
+                        (nd.id == id || Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2))
+                {
+                    Log.d("NiLS", "permanently removing uid:" + nd.uid);
+                    iter.remove();
+                    mDirty = true;
+                }
+                // make sure next time it won't be protected from deleting
+                nd.protect = false;
             }
-            // make sure next time it won't be protected from deleting
-            nd.protect = false;
+        }
+        finally
+        {
+            w.unlock();
         }
     }
 
